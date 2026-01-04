3d2a9237aa37c6e3efacf5b6d316a11bcf02406d
Marcel <marcelrg.business@gmail.com>
2026-01-04 20:13:36 +0700

    Delete obsolete tests

--- test/run-import.test.js
+++
@@ -1,7 +0,0 @@
-import { test } from "node:test";
-import assert from "node:assert";
-
-test("run() can be imported", async () => {
-    const mod = await import("../src/run.js");
-    assert.equal(typeof mod.run, "function");
-});
