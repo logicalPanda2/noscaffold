ac8a83e233271bbeecd166e01c22e448bcaf6c8d
Marcel <marcelrg.business@gmail.com>
2026-01-04 21:13:11 +0700

    Extract eslint config

--- src/run.js
+++ src/run.js
@@ -306,16 +306,48 @@ async function setupTsConfig(rootPath) {
             "include": ["src"]
         }`,
     );
-
 }
 
-async function placeholderName() {
-
+async function setupEslintConfig(rootPath) {
+    console.log("Setting up lint rules...");
+    
     const eslintConfigPath = path.join(rootPath, "eslint.config.js");
 
     await writeFile(
         eslintConfigPath,
-        "import js from '@eslint/js';import globals from 'globals';import reactHooks from 'eslint-plugin-react-hooks';import reactRefresh from 'eslint-plugin-react-refresh';import tseslint from 'typescript-eslint';import { defineConfig, globalIgnores } from 'eslint/config';import reactX from 'eslint-plugin-react-x';import reactDom from 'eslint-plugin-react-dom';export default defineConfig([globalIgnores(['dist']),{files: ['**/*.{ts,tsx}'],extends: [js.configs.recommended,tseslint.configs.recommended,tseslint.configs.strictTypeChecked,tseslint.configs.stylisticTypeChecked,reactHooks.configs.flat.recommended,reactRefresh.configs.vite,reactX.configs['recommended-typescript'],reactDom.configs.recommended,],languageOptions: {parserOptions: {project: ['./tsconfig.node.json', './tsconfig.app.json'],tsconfigRootDir: import.meta.dirname,},ecmaVersion: 2020,globals: globals.browser,},},])",
+        `import js from '@eslint/js';
+        import globals from 'globals';
+        import reactHooks from 'eslint-plugin-react-hooks';
+        import reactRefresh from 'eslint-plugin-react-refresh';
+        import tseslint from 'typescript-eslint';
+        import reactX from 'eslint-plugin-react-x';
+        import reactDom from 'eslint-plugin-react-dom';
+        import { defineConfig, globalIgnores } from 'eslint/config';
+        
+        export default defineConfig([
+            globalIgnores(['dist']),
+            {   
+                files: ['**/*.{ts,tsx}'],
+                extends: [
+                    js.configs.recommended,
+                    tseslint.configs.recommended,
+                    tseslint.configs.strictTypeChecked,
+                    tseslint.configs.stylisticTypeChecked,
+                    reactHooks.configs.flat.recommended,
+                    reactRefresh.configs.vite,
+                    reactX.configs['recommended-typescript'],
+                    reactDom.configs.recommended,
+                ],
+                languageOptions: {
+                    parserOptions: {
+                        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+                        tsconfigRootDir: import.meta.dirname,
+                    },
+                    ecmaVersion: 2020,
+                    globals: globals.browser,
+                },
+            },
+        ])`,
     );
 }
 
